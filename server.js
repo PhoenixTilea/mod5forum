44e54fcf0cb238de7baab02fdd33d447c89126a2
Sabelyn Thorpe[B[D[C[C[D[D[D[D[D[D[D[C[C[C[C[C[C[A[B[B[B[B[B[C[C[D[D[D[D[C[C[C[Sabelyn Thorpe <sabelyn@outlook.com>
2020-09-24 15:00:52 -0400

    Intial commit with some boilerplate and dependencies

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+mongoose.connect("mongodb://localhost:27017/mod-5-forum-db",
+	{useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false},
+).then(() => console.log("Successfully connected to the database"))
+.catch(err => console.log(err));
+
+const app = express();
+app.use(morgan("dev"));
+app.use(express.json());
+
+
+app.use(require("./middle/error"));
+
+const port = process.env.PORT || 8000;
+app.listen(() => console.log(`The server is listening on port ${port}`));
