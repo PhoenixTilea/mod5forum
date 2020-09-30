49c9e99a32a8e531257265107a9bb643464dce27
LarissaBrown <larisa.brown.johnson@gmail.com>
2020-09-30 13:05:13 -0600

    initial commit

---
+++ client/src/components/Menu.js
@@ -0,0 +1,46 @@
+import React, {useState, useEffect, useRef} from "react"
+import User from "./User"
+//import {Link, } from "react-router-dom"
+
+
+
+
+export default function Menu (){
+    const dropdownRef = useRef(null);
+    const [isActive, setIsActive]= useState(false);
+    const onClick = () => setIsActive(!isActive);
+
+    useEffect(()=> {
+        const pageClickEvent = (e) => {
+            // If the active element exists and is clicked outside of
+            if (dropdownRef.current !== null && !dropdownRef.current.contains(e.target)) {
+              setIsActive(!isActive);
+            }
+          };
+        if (isActive) {
+            window.addEventListener('click', pageClickEvent);
+        }
+        return()=> {
+            window.removeEventListener('click', pageClickEvent);
+        }
+     }, [isActive])
+
+     
+
+    return(
+        <>
+             <div className="menu-container">
+             <button onClick={onClick} className="menu-trigger">
+                <img className="menu-img"src= "https://static.thenounproject.com/png/696519-200.png"  alt="menu"/>
+             </button>
+                <User />
+             <nav ref={dropdownRef} className={`menu ${isActive ? 'active' : 'inactive'}`}>
+                <ul>
+                    <li>Category</li>{// needs functionality with mapping here from database of categories}
+}
+                </ul>
+             </nav>
+             </div>
+        </>
+    )
+}
---
+++ client/src/components/Topics.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Topics() {
+    return (
+        <div className="topics-container">
+            
+            
+        </div>
+    )
+}
