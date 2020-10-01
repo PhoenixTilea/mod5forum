1e737591f73319f36ae23ac53fe310c2744bdcb0
LarissaBrown <larisa.brown.johnson@gmail.com>
2020-10-01 13:07:11 -0600

    updated

--- client/src/components/Replies.js
+++ client/src/components/Replies.js
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import Reply from './Reply'
+import AddReplyForm from './AddReplyForm'
 
-export default function Replies() {
-    return (
-        <div className="replies-container">
-            
-            
-        </div>
-    )
+
+export default function Replies(){
+    
+    const [replies, setReplies] = useState([])
+
+function getReplies(){
+    axios.get("/replies")
+    .then(res => {
+        setReplies(res.data)
+    })
+    .catch(error => console.log(error));
 }
+
+function addReply(newReply){
+    axios.post("/protected/replies", newReply)
+    .then(res => {
+        setReplies(previousReplies => [...previousReplies, res.data ] )
+    })
+    .catch(error => console.log(error))
+
+}
+
+useEffect(() => {
+   getReplies()
+},[])
+
+
+return (
+    <div className="replies-container">
+        {replies.map(reply => <Reply {...reply} key={reply.userName} />)}
+        <AddReplyForm addReply={addReply}/>
+    </div>
+)
+}
+
