df147a237755190c608a7d87f732ff479e0ceb22
Larissa Brown <larisa.brown.johnson@gmail.com>
2020-10-27 10:59:09 -0600

    message

--- client/src/context/UserContext.js
+++ client/src/context/UserContext.js
@@ -72,6 +72,7 @@ export default function UserProvider(props) {
         localStorage.removeItem("token")
         localStorage.removeItem("user")
         setUserState(initState)
+
     }
 
     // function getData???????
